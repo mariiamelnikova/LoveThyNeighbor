295e5d3119c94f67e0906fee27679c5888368dbe
Mary Melnikova <mariia.melnikova@optum.com>
2019-11-02 11:06:12 -0500

    added services file

---
+++ client/src/services.js
@@ -0,0 +1,67 @@
+/*
+ * Add your services here
+ */
+export async function callService(path, method, headerObj, bodyObj) {
+    var baseURL = "http://localhost:8080/";
+    var fullURL = baseURL + path;
+
+    var result = {};
+    if (method === "GET") {
+        try {
+            result = await fetch(fullURL, {
+                method: "GET",
+                headers: headerObj
+            });
+        } catch (error) {
+            result = {};
+            result.ok = false;
+            result.url = path;
+            result.statusText = "Service error:  " + path;
+        }
+    } else {
+        try {
+            result = await fetch(fullURL, {
+                method: method,
+                headers: headerObj,
+                body: JSON.stringify(bodyObj)
+            });
+        } catch (error) {
+            result = {};
+            result.ok = false;
+            result.url = path;
+            result.statusText = "Service error:  " + path;
+        }
+    }
+
+    if (result.ok !== true) {
+        var emsg = result.statusText;
+        if (result.status === 404) {
+            emsg = "Service not found:  " + result.url;
+        }
+        const errObj = {
+            error: true,
+            code: result.status,
+            message: emsg,
+            url: result.url
+        };
+        return errObj;
+    } else {
+        const data = await result.json();
+        return data;
+    }
+}
+
+export async function getUsers(userId) {
+    var headerObj = {
+        "Content-Type": "application/json"
+    };
+    var bodyObj = {};
+    var response = await callService(
+        "users/" + userId.toString(),
+        "GET",
+        headerObj,
+        bodyObj
+    );
+
+    return response;
+}
