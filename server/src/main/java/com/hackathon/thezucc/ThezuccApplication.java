b112cd1e53d61ca3c3727b1ed78d06427dd31f55
Procopio <jprof@allstate.com>
2019-11-02 10:20:57 -0500

    moves folder structure for client and server

---
+++ server/src/main/java/com/hackathon/thezucc/ThezuccApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.thezucc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThezuccApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThezuccApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/hackathon/thezucc/ThezuccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.thezucc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThezuccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
