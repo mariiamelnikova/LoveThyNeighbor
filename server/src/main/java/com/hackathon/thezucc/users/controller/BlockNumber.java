26e5902c8b5a7b247cbdc2fbfd77548801b97402
Procopio <jprof@allstate.com>
2019-11-02 13:22:35 -0500

    finishes

---
+++ server/src/main/java/com/hackathon/thezucc/users/controller/BlockNumber.java
@@ -0,0 +1,39 @@
+package com.hackathon.thezucc.users.controller;
+
+import com.hackathon.thezucc.users.model.User;
+import com.hackathon.thezucc.users.repository.UsersRespository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequiredArgsConstructor
+public class BlockNumber {
+
+    private final UsersRespository usersRespository;
+
+    @GetMapping("/users/block/{blockNumber}")
+    public ResponseEntity<List<User>> findByBlockNumber(@PathVariable int blockNumber) {
+
+        try {
+            List<User> users = usersRespository.findAllByBlockNumber(blockNumber);
+
+            if (users == null) {
+                throw new Exception("No users with that block id: " + blockNumber);
+            }
+
+            return new ResponseEntity<>(users, HttpStatus.OK);
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
--- server/src/main/java/com/hackathon/thezucc/users/repository/UsersRespository.java
+++ server/src/main/java/com/hackathon/thezucc/users/repository/UsersRespository.java
@@ -3,7 +3,10 @@ package com.hackathon.thezucc.users.repository;
 import com.hackathon.thezucc.users.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface UsersRespository extends MongoRepository<User, String> {
 
     User findByQrId(long id);
+    List<User>findAllByBlockNumber(long blockNumber);
 }
