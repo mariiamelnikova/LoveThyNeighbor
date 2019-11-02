a2465a4a68d1d8788e306d52669c6aff06fbc1d4
Iqra Memon <imemon3@uic.edu>
2019-11-02 13:37:17 -0500

    changing some ui

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -57,7 +57,7 @@ class Sidebar extends Component {
           )}
         <div className="logo">
           <a
-            href="https://www.creative-tim.com?ref=lbd-sidebar"
+            href="https://www.formyblock.org/"
             className="simple-text logo-mini"
           >
             <div className="logo-img">
@@ -65,10 +65,10 @@ class Sidebar extends Component {
             </div>
           </a>
           <a
-            href="https://www.creative-tim.com?ref=lbd-sidebar"
+            href="https://www.formyblock.org/"
             className="simple-text logo-normal"
           >
-            Creative Tim
+            My Block
           </a>
         </div>
         <div className="sidebar-wrapper">
